0881fdca0adfccc3bbfb69069c462e5822064f0f
jonasaberger <145124362+jonasaberger@users.noreply.github.com>
2025-01-17 13:53:43 +0100

    Add SubscriptionStatus enum and refactor Workspaces entity properties

--- src/workspaces/entities/workspaces.entity.ts
+++ src/workspaces/entities/workspaces.entity.ts
@@ -1,81 +1,31 @@
 /* eslint-disable prettier/prettier */
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
+export enum SubscriptionStatusEnum {
+  Unpaid = 'Unpaid',
+  Canceled = 'Canceled',
+  Active = 'Active',
+  SessionPending = 'SessionPending'
+}
+
 @Entity("Workspaces")
 export class Workspaces {
+
   @PrimaryGeneratedColumn() // Auto-incremented primary key
   Id : number;
-  @Column()
-  Name: string;
-  @Column()
-  NameEn : string;
-  @Column()
-  Description : string;
-  @Column()
-  DescriptionEn : string;
-  @Column()
-  ContactUrl : string;
-  @Column()
-  ContactMail : string;
-  @Column()
-  ContactPhone : string;
-  @Column()
-  FirstName : string;
-  @Column()
-  LastName : string;
-  @Column()
-  DateOfBirth : Date;
-  @Column()
-  AddressLine1 : string;
-  @Column()
-  AddressLine2 : string;
-  @Column()
-  PostalCode : number;
-  @Column()
-  City : string;
-  @Column()
-  CountryId : number;
-  @Column()
-  CompanyName : string;
-  @Column()
-  CompanyVat : string;
-  @Column()
-  CompanyDataProtectionUrl : string;
-  @Column()
-  UploadLimit : boolean;
-  @Column()
-  StripeSubscriptionId : number;
-  @Column()
-  StripeSessionId : number;
-  @Column()
-  SubscriptionStatus : boolean;
-  @Column()
-  SubscriptionEndDate : Date;
-  @Column()
-  TrialStartOn : Date;
-  @Column()
-  TrialEndsOn : Date;
-  @Column()
-  OwnerId : number;
-  @Column()
-  FileId : number;
-  @Column()
-  DefaultPrizeId : number;
-  @Column()
-  HasPassword : boolean;
-  @Column()
-  Password : string;
-  @Column()
-  Slug : string;
-  @Column()
-  CreatedById : number;
-  @Column()
-  CreatedOn : Date;
-  @Column()
-  LastModifiedById : number;
-  @Column()
-  LastModifiedOn : Date
 
+  @Column()
+  Name: string; // Name des entsprechenden Workspace
 
+  @Column()
+  Description : string; // Deskription des Workspaces für evlt. Implementation
 
+  @Column()
+  CompanyName : string; // Name des entsprechenden Unternehmens
+
+  @Column({
+    type: 'enum',
+    enum: SubscriptionStatusEnum
+  })
+  SubscriptionStatus : SubscriptionStatusEnum; // Enumerator für SubscriptionStatus
 }
